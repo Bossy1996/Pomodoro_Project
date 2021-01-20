507939d9ce4d0f79ef7d2d545edc3199cb9d0e32
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-01-20 16:02:03 +0100

    Add Example 3

--- src/examples/example3.py
+++ src/examples/example3.py
@@ -1,36 +1,34 @@
 import sys
+
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QToolBar, QAction, QStatusBar
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QSize
 
-
-# Subclass QMainWindow to customise your application's main window
 
 class MainWindow(QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
-        self.setWindowTitle("My second try on PyQt5")
+        self.setWindowTitle("My Awesome App")
 
-        label = QLabel("This is a PyQt5 window!")
-
-        # The 'Qt' name space has a lot of attributes to customise
-        # widgets. See: http://doc.qt.io/qt-5/qt.html
+        label = QLabel("THIS IS AWESOME!!!")
         label.setAlignment(Qt.AlignCenter)
 
-        # Set the central widget of the window. Widget will expand
-        # to take up all the space in the window by default
         self.setCentralWidget(label)
 
         toolbar = QToolBar("My main toolbar")
+        toolbar.setIconSize(QSize(16,16))
         self.addToolBar(toolbar)
 
-        button_action = QAction("your buttom", self)
+        button_action = QAction(QIcon("bug.png"), "Your button", self)
         button_action.setStatusTip("This is your button")
         button_action.triggered.connect(self.onMyToolBarButtonClick)
+        button_action.setCheckable(True)
         toolbar.addAction(button_action)
 
         self.setStatusBar(QStatusBar(self))
+
 
     def onMyToolBarButtonClick(self, s):
         print("click", s)
