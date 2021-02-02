e3c199a491a0d84ce932cdeff9fd39ea468df398
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-02-02 13:47:18 +0100

    Ui - base ui done

--- src/main.py
+++ src/main.py
@@ -10,78 +10,40 @@ class MainWindow(QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-        # by default a Pomodoro has 4 blocks of work
-        self.pomodoroCount = 4
-        self.pomodoro_stage = ""
-        # count down thingy
+        # by default a Pomodoro has 4 blocks of work and in each block there are different stages
+        # like Pomodoro (working time) short-break (short rest) and long break (long rest)
+        self.pomodoroCount = 0
+        # Time counter
         self.count = 0
         # Boolean that control if the timer has started or not
         self.start = False
 
-        # Set window title
+        # Set buttons title
         self.setWindowTitle("Pomodoro Focus")
 
-        # Set the size of the window
-        # self.setGeometry(100, 100, 400, 600)
+        # Set central Widget as a MainWindow it must have a Central widget
+        self.centralWidget = QWidget()
+        self.setCentralWidget(self.centralWidget)
 
         # components of the ui
-        self.user_ui()
-
+        grid_layout = QGridLayout()
+        pomodoro_button = QPushButton("Pomodoro")
+        short_break_button = QPushButton("Short Break")
+        long_break_button = QPushButton("Long Break")
+        place_holder = QLabel("Place Holder")
+        place_holder.setStyleSheet("border: 3px solid black")
+        place_holder.setFont(QFont("Times", 15))
+        start_button = QPushButton("Start")
+        settings_button = QPushButton("Settings")
+        grid_layout.addWidget(pomodoro_button, 0, 0)
+        grid_layout.addWidget(short_break_button, 0, 1)
+        grid_layout.addWidget(long_break_button, 0, 2)
+        grid_layout.addWidget(place_holder, 1, 1)
+        grid_layout.addWidget(start_button, 2, 1)
+        grid_layout.addWidget(settings_button, 3, 1)
+        self.centralWidget.setLayout(grid_layout)
         # shows all the widgets
         self.show()
-
-    def user_ui(self):
-        # we declare the buttons
-        pomodoro_button = QPushButton("Pomodoro", self)
-        short_break_button = QPushButton("Short-Break", self)
-        long_break_button = QPushButton("Long_break", self)
-        start_button = QPushButton("Start", self)
-        settings = QPushButton("Settings", self)
-
-        # we create the label timer
-        time_label = QLabel("Place-Holder", self)
-        time_label.setStyleSheet("border: 3px solid black")
-        time_label.setFont(QFont('times', 15))
-        time_label.setAlignment(Qt.AlignCenter)
-
-        # we add the buttons to the grid layout
-        grid = QGridLayout()
-        grid.addWidget(pomodoro_button, 0, 0)
-        grid.addWidget(short_break_button, 0, 1)
-        grid.addWidget(long_break_button, 0, 2)
-        grid.addWidget(start_button, 2, 1)
-        grid.addWidget(settings, 3, 1)
-        grid.addWidget(time_label, 1, 1)
-
-        # we add functionality to the buttons
-        pomodoro_button.clicked.connect(self.pomodoro)
-        short_break_button.clicked.connect(self.short_break)
-        long_break_button.clicked.connect(self.long_break)
-        start_button.clicked.connect(self.start_pomodoro)
-        settings.clicked.connect(self.settings)
-
-
-
-        # we add it to the grid layout
-
-
-        # main_layout = QHBoxLayout()
-
-        # main_layout.addWidget(QPushButton("Pomodoro"))
-        # main_layout.addWidget(QPushButton("Short break"))
-        # main_layout.addWidget(QPushButton("Long Break"))
-
-        # widget = QWidget()
-        # widget.setLayout(main_layout)
-        # widget.setGeometry(125, 100, 150, 50)
-
-        # layout = QGridLayout()
-        # layout.addWidget(QPushButton("Pomodoro"), 0, 0)
-        # layout.addWidget(QPushButton("Short-break"), 0, 1)
-        # layout.addWidget(QPushButton("Long-break"), 0, 2)
-        #
-        # widget = QWidget()
-        # widget.setLayout(layout)
 
     def start_pomodoro(self):
         pass
@@ -113,14 +75,9 @@ class MainWindow(QMainWindow):
         timeformat = '{:02d}:{:02d}'.format(mins, secs)
         time.sleep(1)
         t -= 1
+
+
 # TODO: Info button that shows you in the browser what is the Pomodoro technique
-
-
-
-
-
-
-
 
 
 # You need one (and only one) QApplication instance per application
@@ -129,7 +86,7 @@ class MainWindow(QMainWindow):
 app = QApplication(sys.argv)
 
 window = MainWindow()
-# window.show()  # IMPORTANT !!! Windows are hidden by default.
+window.show()  # IMPORTANT !!! Windows are hidden by default.
 
 # Start the event loop.
 app.exec()
